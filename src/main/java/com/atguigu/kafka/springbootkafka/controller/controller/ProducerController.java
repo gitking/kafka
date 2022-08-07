40ccef9f12e591d2522d6b71c1d8e5e37dd5e68d
gitking <yale268sh@163.com>
2022-08-07 15:47:00 +0800

    SpringBoot集成Kafka

--- src/main/java/com/atguigu/kafka/springbootkafka/controller/controller/ProducerController.java
+++ src/main/java/com/atguigu/kafka/springbootkafka/controller/controller/ProducerController.java
@@ -5,6 +5,11 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * https://www.bilibili.com/video/BV1vr4y1677k?p=71&vd_source=acb9b66ebe2b9150f655070a930aeb12
+ * 《【尚硅谷】2022版Kafka3.x教程（从入门到调优，深入全面）》  《 P71 71_尚硅谷_Kafka_集成_SpringBoot生产者  09:35》
+ * 2022年8月7日10:05:30
+ */
 @RestController
 public class ProducerController {
 
